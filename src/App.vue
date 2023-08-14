43a9e7f940474ef1e8ad654df3c124c1e505e10d
user1808 <tomasz.kozak04@gmail.com>
2023-08-14 13:01:40 +0200

    my-switch feat: create my-switch component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div>TEST</div>
-  <MySwitch v-model="value" />
+  <MySwitch v-model="value" color="red" />
   {{ value }}
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import MySwitch from './components/MySwitch.vue';
+import MySwitch from './components/MySwitch';
 
 const value = ref<boolean>(true);
 </script>
---
+++ src/components/MySwitch/types.ts
@@ -0,0 +1,5 @@
+export type TModelValue = boolean;
+export type TColor = string;
+export type TSize = number;
+export type TInset = boolean;
+export type TDisabled = boolean;
